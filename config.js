0d8179b68838dce5a8294960daddf07f63c1703e
Sarada <saradawrites@gmail.com>
2026-08-16 20:27:59 +0530

    Update API_URL to the deployed Google Apps Script URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Example: https://script.google.com/macros/s/ABC123/exec
  */
 window.FAMILY_DASHBOARD_CONFIG = Object.freeze({
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycbw5N_RPO7KU7xrsBvMtapqR9YwDqrfn5zQj9Dk1pWdHhHJUMVSIlbPLh2_Wq_hemyCxxg/exec",
   REQUEST_TIMEOUT_MS: 60000,
   APP_VERSION: "1.0.1"
 });

a82b9cd5d070aef03d03d5504f18db9c690c7abd
Sarada <saradawrites@gmail.com>
2026-08-22 23:48:46 +0530

    Update API_URL in config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Example: https://script.google.com/macros/s/ABC123/exec
  */
 window.FAMILY_DASHBOARD_CONFIG = Object.freeze({
-  API_URL: "https://script.google.com/macros/s/AKfycbxSc7zn5rYAziQVC8bLIOIzcXd9PEAGT8J2K0ocFfSm_ym9Xsj8kWiSAewz0-jg-wMO/exec",
+  API_URL: "https://script.google.com/macros/s/AKfycby4YPjQL165mIQgKjSHR4DX7EHjORbskRQL-Leyr5hCz0lD-d_0P93fPvyqgJx1HjbpPw/exec",
   REQUEST_TIMEOUT_MS: 60000,
   APP_VERSION: "1.0.8"
 });

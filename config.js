b16c3fc32d8c0af1690bce245fbe30dc7b74833c
Sarada <saradawrites@gmail.com>
2026-08-23 00:13:28 +0530

    Update API_URL in config.js to production URL

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
  * Example: https://script.google.com/macros/s/ABC123/exec
  */
 window.FAMILY_DASHBOARD_CONFIG = Object.freeze({
-  API_URL: "PASTE_YOUR_APPS_SCRIPT_WEB_APP_URL_HERE",
+  API_URL: "https://script.google.com/macros/s/AKfycby4YPjQL165mIQgKjSHR4DX7EHjORbskRQL-Leyr5hCz0lD-d_0P93fPvyqgJx1HjbpPw/exec",
   REQUEST_TIMEOUT_MS: 60000,
   APP_VERSION: "1.0.9"
 });

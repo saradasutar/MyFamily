4f8fca7ba17cc64712fd20478134765c6cd88690
Sarada <saradawrites@gmail.com>
2026-08-29 20:16:05 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,9 @@
-window.PORTFOLIO_CONFIG = {
-  API_URL: 'https://script.google.com/macros/s/AKfycbwXA1YBWRfOjYvZwDz9dkCr4HZujn9D_YoPmQyjggZxR5hUUs1ZsARytOX8rUR4zRbs/exec',
-  APP_NAME: 'SarNi',
-  CURRENCY: 'INR',
+/*
+ * Paste the deployed Google Apps Script Web App /exec URL below.
+ * Example: https://script.google.com/macros/s/ABC123/exec
+ */
+window.FAMILY_DASHBOARD_CONFIG = Object.freeze({
+  API_URL: "https://script.google.com/macros/s/AKfycbxz7pKfGI4tIgHxIfW2CcRjURoA0vk9qFZvLXVMrmm_k4x2R49wEdExn_n4usYy9QOV1w/exec",
   REQUEST_TIMEOUT_MS: 60000,
-  OWNERS: ['Sarada', 'Niharika']
-};
+  APP_VERSION: "1.0.23"
+});
